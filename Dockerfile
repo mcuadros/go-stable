921aa342f1d7958fd7a278d44feb018b1d65358d
Máximo Cuadros <mcuadros@gmail.com>
2015-12-05 12:49:29 +0100

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.5.2-alpine
+MAINTAINER Máximo Cuadros <mcuadros@gmail.com>
+
+RUN apk -U add git && rm -rf /var/cache/apk/*
+
+ADD . ${GOPATH}/src/github.com/mcuadros/gop.kg
+WORKDIR ${GOPATH}/src/github.com/mcuadros/gop.kg
+RUN go get -v ./...
+RUN go install -v ./...
+
+VOLUME /certificates
+EXPOSE 80
+
+CMD ["gop.kg", "server", "--addr=:80", "--cert=/certificates/gop.kg.cert.pem", "--key=/certificates/gop.kg.key.pem"]
--- cli/gop.kg/cmd_server.go
+++ cli/gop.kg/cmd_server.go
@@ -3,12 +3,13 @@ package main
 import "github.com/mcuadros/gop.kg"
 
 type ServerCommand struct {
-	Addr string `long:"addr" default:":8080" description:"http server addr"`
-
-	s *gopkg.Server
+	Addr     string `long:"addr" default:":8080" description:"http server addr"`
+	CertFile string `long:"cert" description:"TLS certificate file path."`
+	KeyFile  string `long:"key" description:"TLS key file path."`
+	s        *gopkg.Server
 }
 
 func (c *ServerCommand) Execute(args []string) error {
 	c.s = gopkg.NewServer()
-	return c.s.Run(c.Addr)
+	return c.s.RunTLS(c.Addr, c.CertFile, c.KeyFile)
 }
